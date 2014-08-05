5e06eae1a5f81a7fd18b12f8e5ab85cb9a078ae8
João Jr. Casa <joao29a@gmail.com>
2014-08-05 16:54:48 -0300

    fixed

---
+++ src/main/webapp/js/menu.js
@@ -0,0 +1,42 @@
+var submenus = ['#sub-menu1'];
+
+var Menu = {
+    close_submenus: function(){
+        for (var i = 0; i < submenus.length; i++){
+            $(submenus[i]).hide();
+        }
+    }
+}
+
+$(document).ready(function(){
+    $('#content').load('public/home.html');
+
+    $('#home').click(function(){
+        Menu.close_submenus();
+        $('#content').load('public/home.html');
+    });
+
+    $('#opt1').click(function(){
+        $('#sub-menu1').toggle();
+    });
+
+    $('#sub1-opt1').click(function(){
+        $('#content').load('public/test.html');
+    });
+
+    $('#hide-top').click(function(){
+        $('#login-menu').hide();
+    });
+
+    $('#show-top').click(function(){
+        $('#login-menu').toggle();
+    });
+
+    $('#login').click(function(){
+        $('#content').load('public/login.html');
+    });
+
+    $('#register').click(function(){
+        $('#content').load('public/register.html');
+    });
+});
